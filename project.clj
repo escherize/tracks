885217895800d761dce9ee47b98ec93cd1605d21
Bryan Maass <bryan.maass@gmail.com>
2016-10-11 23:15:31 +1100

    add url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject tracks "0.1.1"
+(defproject tracks "0.1.2"
   :description "Example based function generation"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/escherize/tracks"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
