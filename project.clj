27148279fee52940562eadea43ddc2bbb8fe5d86
Bryan Maass <bryan.maass@gmail.com>
2016-10-11 22:59:39 +1100

    bump version

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject tracks "0.1.0"
-  :description "Make functions in a declarative manner"
+(defproject tracks "0.1.1"
+  :description "Example based function generation"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
