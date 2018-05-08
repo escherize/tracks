6949a5ff30fef9227ff1643094627ee00002e81d
Bryan Christopher Maass <bryan.maass@gmail.com>
2018-05-07 22:13:40 -0500

    bump version

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject tracks "1.0.4"
+(defproject tracks "1.0.5"
   :description "Example based function generation"
   :url "https://github.com/escherize/tracks"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]
                                   [viebel/codox-klipse-theme "0.0.1"]]}}
   :jvm-opts ["-XX:-OmitStackTraceInFastThrow"])
